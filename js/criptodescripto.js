d6937c4182d9cd4ec306e7ede0bf265d551d0d85
viniciusSouza30 <viniciussmti@gmail.com>
2022-01-21 10:37:44 -0300

    criptografando as mensagens

--- js/criptodescripto.js
+++ js/criptodescripto.js
@@ -1,3 +1,37 @@
 $(window).on("load", function () {
   $("#modal-sobre-o-programa").modal("show");
 });
+
+function criptografar() {
+
+  
+}
+
+document.getElementById("cripto").addEventListener("click", function () {
+    let mensagem = document.getElementById("mensagem").value;
+    let mensagemAuxiliar = mensagem.split("");
+
+    for (let i = 0; i < mensagemAuxiliar.length; i++) {
+      if (mensagemAuxiliar[i] === "e") {
+        mensagemAuxiliar[i] = "enter";
+      } else if (mensagemAuxiliar[i] === "i") {
+        mensagemAuxiliar[i] = "imes";
+      } else if (mensagemAuxiliar[i] === "a") {
+        mensagemAuxiliar[i] = "ai";
+      } else if (mensagemAuxiliar[i] === "o") {
+        mensagemAuxiliar[i] = "obter";
+      } else if (mensagemAuxiliar[i] === "u") {
+        mensagemAuxiliar[i] = "ufat";
+      }
+    }
+
+    alert(mensagemAuxiliar.join(''));
+});
+
+document.getElementById("descripto").addEventListener("click", function () {
+    let mensagem = document.getElementById("mensagem").value;
+    let mensagemAuxiliar = mensagem.split("");
+
+
+    alert(mensagemAuxiliar.join(''));
+});
